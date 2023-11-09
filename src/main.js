5a5f3b4ca898ee0a11e7b14e1b893ad139e879b8
n3kr4s0v4 <84867701+n3kr4s0v4@users.noreply.github.com>
2023-11-09 20:18:21 +0300

    тест

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
+import Splide from '@splidejs/splide';
+/*document.addEventListener("DOMContentLoaded", function () {
+  const agePopup = document.getElementById("age-confirmation-popup");
+  const ageInput = document.getElementById("age-input");
+  const confirmBtn = document.getElementById("confirm-btn");
 
+  confirmBtn.addEventListener("click", function () {
+    const age = parseInt(ageInput.value);
+
+    if (age >= 18) {
+      agePopup.style.display = "none";
+    } else {
+      alert("Вы должны быть старше 18 лет!");
+    }
+  });
+});*/
+
+
+const mySlider = new Splide('.splide');
+mySlider.mount();
