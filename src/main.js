50ce9b1243558a5a8d0584a87820ab31319706d9
MariaZh23 <mzh083@gmail.com>
2023-11-13 23:19:18 +0300

    Header and Footer (adaptive, with burger-menu)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+let menuBtn = document.querySelector('.burger-menu__btn');
+let menu = document.querySelector('.header-block__bm-list');
 
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+});
