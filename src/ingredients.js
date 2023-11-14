cca5ac1f21d54c395816831fab789bebf48a485e
Evgenia0712 <e.gunko1992@mail.ru>
2023-11-14 23:29:26 +0300

    работаю над модальным окном

--- src/ingredients.js
+++ src/ingredients.js
@@ -17,6 +17,17 @@ function testGetData() {
         console.log(obj);
         print(key, obj.name, obj.cocktails, obj.img);
       }
+      let coctailsElement = document.getElementsByClassName("container_item");
+
+      // Добавление обработчика события клика к каждому ингредиенту
+      Array.from(coctailsElement).forEach(function (container_item) {
+        container_item.addEventListener('click', function () {
+          let name = this.querySelector('.container_item__ingredient').textContent;
+          let description = this.querySelector('.container_item__coctails').textContent;
+
+          showModal(name, description);
+        });
+      });
     })
     .catch(error => {
       // обрабатываем ошибку, если она возникла
@@ -32,7 +43,7 @@ function print(id, name, cocktails, img) {
     <img class="container_item__image" src="${img}" alt="">
     <div class="container_item__ingredient">${name}</div>
     <div class="container_item__coctails"><span>Содержится в коктейлях:</span><br> ${cocktails}</div>
-    </div>
+  </div>
     `
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
@@ -40,3 +51,39 @@ function print(id, name, cocktails, img) {
 }
 
 testGetData()
+
+let modalElement = document.getElementById('modal');
+// Функция отображения модального окна
+function showModal(name, description) {
+  modalElement.style.display = 'block';
+  modalElement.textContent = 'Информация о товаре:\n'
+}
+
+
+
+/*
+// Функция для обработки события клика
+function showInfo() {
+  // Создаем новый элемент div с информацией
+  const infoElement = document.createElement('div');
+  infoElement.textContent = 'Дополнительная информация о коктейлях'; // Вместо этого текста можно добавить свою информацию
+
+  // Добавляем класс для стилизации
+  infoElement.classList.add('additional-info');
+
+  // Добавляем информацию внутрь элемента container
+  const containerElement = document.getElementById('container');
+  containerElement.appendChild(infoElement);
+}
+
+// Получаем элемент, на который нужно добавить обработчик события
+const cocktailsElement = document.querySelectorAll('.container_item__coctails');
+
+// Добавляем обработчик события клика
+cocktailsElement.addEventListener('click', showInfo);
+*/
+
+
+
+
+
