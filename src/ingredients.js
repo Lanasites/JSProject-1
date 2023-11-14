4717e39a31c1a5931cd81f2727b7b5a62432c691
Evgenia0712 <e.gunko1992@mail.ru>
2023-11-15 01:59:07 +0300

    работа над модальным окном с информацией

--- src/ingredients.js
+++ src/ingredients.js
@@ -25,7 +25,8 @@ function testGetData() {
           let name = this.querySelector('.container_item__ingredient').textContent;
           let description = this.querySelector('.container_item__coctails').textContent;
 
-          showModal(name, description);
+          //showModal(id, name, description, cooking, img);
+          showModal();
         });
       });
     })
@@ -52,36 +53,96 @@ function print(id, name, cocktails, img) {
 
 testGetData()
 
-let modalElement = document.getElementById('modal');
+// Функции для заполнения модального окна
+
+const modalElement = document.getElementById('modal');
+
 // Функция отображения модального окна
-function showModal(name, description) {
+function showModal() {
   modalElement.style.display = 'block';
-  modalElement.textContent = 'Информация о товаре:\n'
+  testGetDataModal();
+  // const newElement = document.createElement('div');
+  // newElement.innerHTML = template;
+  // modalElement.appendChild(newElement);
+  // //modalElement.textContent = 'Информация о товаре:\n'
 }
 
+function testGetDataModal() {
+  console.log('Функция загрузки данных вызвана');
+  // получение промиса
+  const promise = takeAllObjects('cocktails');
+  // console.log(promise);
+  promise
+    // .then(response => response.json())
+    .then(data => {
+      // работаем с полученными данными - объект с объектами
+      // console.log(data);
+      for (let key in data) {
+        // obj - коктель со всеми свойствами
+        let obj = data[key];
+        // console.log(key.trim);
+        printModal(key, obj.name, obj.description, obj.cooking, obj.imageUrl);
+        // так как ингредиенты в массиве хранятся, то их отдельно считываем, чтобы сделать список
+        let ingrArr = obj.ingredients;
+        // console.log(ingrArr);
+        const ingrList = printSostav(ingrArr);
+        const ingr = document.getElementById(key);
 
-
-/*
-// Функция для обработки события клика
-function showInfo() {
-  // Создаем новый элемент div с информацией
-  const infoElement = document.createElement('div');
-  infoElement.textContent = 'Дополнительная информация о коктейлях'; // Вместо этого текста можно добавить свою информацию
-
-  // Добавляем класс для стилизации
-  infoElement.classList.add('additional-info');
-
-  // Добавляем информацию внутрь элемента container
-  const containerElement = document.getElementById('container');
-  containerElement.appendChild(infoElement);
+        if (ingr !== null) {
+          ingr.innerHTML = ingrList;
+          // console.log(ingr);
+        }
+        else {
+          throw new Error('Элемента в БД не найдено');
+        }
+      }
+    })
+    .catch(error => {
+      // обрабатываем ошибку, если она возникла
+      console.error(error);
+    });
 }
 
-// Получаем элемент, на который нужно добавить обработчик события
-const cocktailsElement = document.querySelectorAll('.container_item__coctails');
+function printModal(id, name, description, cooking, img) {
+  //const printElement = document.getElementById('print');
+  const template = `
+  <div class="modal_item">
+  <div class="modal_item-1">
+  <img class="modal_item-1__img" src="${img}" alt="">
+  </div>
+  <div class="modal_item-2">
+  <div class="modal_item-2__name">Название: ${name}</div>
+  <div class="modal_item-2__description"><span>Описание:</span> ${description}</div>
+  <div class="modal_item-2__cooking"><span>Рецепт:</span> ${cooking}</div>
+  <ul class="modal_item-2__id" id = ${id}></ul>
+  </div>
+  </div>
+  `
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+  modalElement.appendChild(newElement);
+}
 
-// Добавляем обработчик события клика
-cocktailsElement.addEventListener('click', showInfo);
-*/
+function printSostav(ingrArr) {
+  // console.log(ingrArr);
+  // захотим в массив из ингредиентов для коктеля, чтобы взять свойства
+  let item = '';
+  for (let i = 0; i < ingrArr.length; i++) {
+    // console.log(ingrArr[i]);
+    let kolvo = ingrArr[i].quantity;
+    let name = ingrArr[i].name;
+
+    item += `<li> ${name}: ${kolvo}</li>`;
+
+  }
+  // console.log('item', item);
+  return item;
+}
+
+document.querySelector('.btn_close').addEventListener('click', function () {
+  modalElement.style.display = 'none';
+})
+
 
 
 
