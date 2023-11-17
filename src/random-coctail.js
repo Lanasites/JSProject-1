49aae5b742fd3b43fd5cb8e60f9dcca67b6b6bc0
vakal-y <dugintseva.y@yandex.ru>
2023-11-17 15:31:46 +0300

    не работает переход

--- src/random-coctail.js
+++ src/random-coctail.js
@@ -1,15 +1,15 @@
 import cocktail from '../js-project-cocktails.json';
 
-// рандомный индекс для коктейля
-// function getRandomCocktail() {
-//     const randomIndex = Math.floor(Math.random() * cocktail.length);
-//     return cocktail[randomIndex];
-// }
+//рандомный индекс для коктейля
+function getRandomCocktail() {
+    const randomIndex = Math.floor(Math.random() * cocktail.length);
+    return cocktail[randomIndex];
+}
 
 // добавление инфы о коктейле на страницу самого коктейля
 
 function showRandomCocktail() {
-    //const randomCocktail = getRandomCocktail();
+    const randomCocktail = getRandomCocktail();
 
     const cocktailImage = document.querySelector('.container-wrapper__pic-img');
     const cocktailName = document.getElementById('name');
@@ -17,32 +17,32 @@ function showRandomCocktail() {
     const cocktailIngredients = document.getElementById('ingredients');
     const cocktailCooking = document.getElementById('cooking');
 
-    const specificCocktail = cocktail['Bumblebee'];
-    //cocktailImage.src = randomCocktail.imageUrl;
-    //cocktailImage.alt = randomCocktail.idcocktail;
+    //const specificCocktail = cocktail['Bumblebee'];
+    cocktailImage.src = randomCocktail.imageUrl;
+    cocktailImage.alt = randomCocktail.idcocktail;
 
-    cocktailImage.src = specificCocktail.imageUrl;
-    cocktailImage.alt = specificCocktail.idcocktail;
+    //cocktailImage.src = specificCocktail.imageUrl;
+    //cocktailImage.alt = specificCocktail.idcocktail;
 
     const itemName = document.createElement('p');
-    //itemName.textContent = randomCocktail.name;
-    itemName.textContent = specificCocktail.name;
+    itemName.textContent = randomCocktail.name;
+    //itemName.textContent = specificCocktail.name;
 
     const itemDescription = document.createElement('p');
-    //itemDescription.textContent = randomCocktail.description;
-    itemDescription.textContent = specificCocktail.description;
+    itemDescription.textContent = randomCocktail.description;
+    //itemDescription.textContent = specificCocktail.description;
 
     const itemIngredients = document.createElement('ul');
-    //randomCocktail.ingredients.forEach(ingredient => {
-    specificCocktail.ingredients.forEach(ingredient => {
+    randomCocktail.ingredients.forEach(ingredient => {
+        //specificCocktail.ingredients.forEach(ingredient => {
         const listItem = document.createElement('li');
         listItem.textContent = `${ingredient.name}: ${ingredient.quantity}`;
         itemIngredients.appendChild(listItem);
     });
 
     const itemCooking = document.createElement('p');
-    //itemCooking.textContent = randomCocktail.cooking;
-    itemCooking.textContent = specificCocktail.cooking;
+    itemCooking.textContent = randomCocktail.cooking;
+    //itemCooking.textContent = specificCocktail.cooking;
 
 
     cocktailName.appendChild(itemName);
