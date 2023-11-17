07118d630cf0a7af8c425ac21225a23b77c435c1
MariaZh23 <mzh083@gmail.com>
2023-11-17 23:44:14 +0300

    some corrections in index, prof-and-fav, add favourities.html (+scss+js)

--- src/profile-and-favourites.js
+++ src/profile-and-favourites.js
@@ -1,22 +1,25 @@
-document.getElementById("profile").addEventListener("click", function() {
-    let menu = document.getElementsByClassName("user-profile__btn");
-    if (menu.style.display === "none") {
-      menu.style.display = "block";
-    } else {
-      menu.style.display = "none";
-    }
-  });
+import { burgerMenu } from './header.js'; 
+burgerMenu();
 
-//Вывод алфавита (букв-ссылок) на страницу
-function AddLetters () {
-const alphabet = 'абвгдежзиклмнопрстуфхцчшщэюя';
-const alphabetDiv = document.getElementById('alphabet');
-for (let i = 0; i < alphabet.length; i++) {// Проходимся по каждой букве в алфавите
-    let letterLink = document.createElement('a');// Создаем ссылку для каждой буквы
-    letterLink.href = '#' + alphabet[i]; // Устанавливаем ссылку для перехода на конкретную букву
-    letterLink.innerHTML = alphabet[i].toUpperCase(); // Устанавливаем текст ссылки
-    alphabetDiv.appendChild(letterLink);// Добавляем ссылку в блок алфавита
-}
-alphabetDiv.classList.add("LetterStyle");
-};
-AddLetters();
+// document.getElementById("profile").addEventListener("click", function() {
+//     let menu = document.getElementsByClassName("user-profile__btn");
+//     if (menu.style.display === "none") {
+//       menu.style.display = "block";
+//     } else {
+//       menu.style.display = "none";
+//     }
+//   });
+
+// //Вывод алфавита (букв-ссылок) на страницу
+// function AddLetters () {
+// const alphabet = 'абвгдежзиклмнопрстуфхцчшщэюя';
+// const alphabetDiv = document.getElementById('alphabet');
+// for (let i = 0; i < alphabet.length; i++) {// Проходимся по каждой букве в алфавите
+//     let letterLink = document.createElement('a');// Создаем ссылку для каждой буквы
+//     letterLink.href = '#' + alphabet[i]; // Устанавливаем ссылку для перехода на конкретную букву
+//     letterLink.innerHTML = alphabet[i].toUpperCase(); // Устанавливаем текст ссылки
+//     alphabetDiv.appendChild(letterLink);// Добавляем ссылку в блок алфавита
+// }
+// alphabetDiv.classList.add("LetterStyle");
+// };
+// AddLetters();
