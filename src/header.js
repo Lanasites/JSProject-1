7418217e311cd7473808d23b8d7d538da71fc750
MariaZh23 <mzh083@gmail.com>
2023-11-17 17:00:51 +0300

    export/import burger, add href "random-coctail.html"

--- src/header.js
+++ src/header.js
@@ -1,12 +1,16 @@
 //Добавление навигации бургер-меню
+function burgerMenu (){
 let menuBtn = document.querySelector('.burger-menu__btn');
 let menu = document.querySelector('.header-block__bm-list');
 menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 });
+}
 
-//Переходы между страницами
+export { burgerMenu };
+
+//Переходы с кнопок
 // //--переход на страницу личного кабинета--//
 // document.getElementById('profile').addEventListener("click", goToPersonalAccount);
 // function goToPersonalAccount (){
