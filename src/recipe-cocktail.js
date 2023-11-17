298a2682f87dd22947272e2adf8fe195016c8b93
vakal-y <dugintseva.y@yandex.ru>
2023-11-17 13:29:27 +0300

    колдую над кодом страницы с рецептом. не отображается инфа на странице

--- src/recipe-cocktail.js
+++ src/recipe-cocktail.js
@@ -6,7 +6,7 @@ import { writeCocktailData } from './firebase.js'
 
 // добавление инфы о коктейле на страницу самого коктейля
 
-function showCoctail() {
+function showCocktail() {
     const cocktailImage = document.querySelector('.container-wrapper__pic-img');
     const cocktailName = document.getElementById('name');
     const cocktailDescription = document.getElementById('description');
@@ -14,8 +14,9 @@ function showCoctail() {
     const cocktailCooking = document.getElementById('cooking');
 
     const specificCocktail = cocktail['Mojito'];
-    cocktailImage.src = specificCocktail.image;
-    cocktailImage.alt = specificCocktail.idcocktail;
+    const itemImage = document.createElement('img');
+    itemImage.src = specificCocktail.imageUrl;
+    itemImage.alt = specificCocktail.idcocktail;
 
     const itemName = document.createElement('p');
     itemName.textContent = specificCocktail.name;
@@ -33,57 +34,57 @@ function showCoctail() {
     const itemCooking = document.createElement('p');
     itemCooking.textContent = specificCocktail.cooking;
 
+    cocktailImage.appendChild(itemImage);
     cocktailName.appendChild(itemName);
     cocktailDescription.appendChild(itemDescription);
     cocktailIngredients.appendChild(itemIngredients);
     cocktailCooking.appendChild(itemCooking);
-
 };
 
-showCoctail();
+showCocktail();
 
 
-// рандомный коктейль
-function getRandomCocktail() {
-    const randomIndex = Math.floor(Math.random() * cocktail.length);
-    return cocktail[randomIndex];
-}
+// // рандомный коктейль
+// function getRandomCocktail() {
+//     const randomIndex = Math.floor(Math.random() * cocktail.length);
+//     return cocktail[randomIndex];
+// }
 
-function showRandomCocktail() {
-    const cocktailImage = document.querySelector('.container-wrapper__pic-img');
-    const cocktailName = document.getElementById('name');
-    const cocktailDescription = document.getElementById('description');
-    const cocktailIngredients = document.getElementById('ingredients');
-    const cocktailCooking = document.getElementById('cooking');
+// function showRandomCocktail() {
+//     const cocktailImage = document.querySelector('.container-wrapper__pic-img');
+//     const cocktailName = document.getElementById('name');
+//     const cocktailDescription = document.getElementById('description');
+//     const cocktailIngredients = document.getElementById('ingredients');
+//     const cocktailCooking = document.getElementById('cooking');
 
-    const randomCocktail = getRandomCocktail();
+//     const randomCocktail = getRandomCocktail();
 
-    cocktailImage.src = randomCocktail.image;
-    cocktailImage.alt = randomCocktail.idcocktail;
+//     cocktailImage.src = randomCocktail.image;
+//     cocktailImage.alt = randomCocktail.idcocktail;
 
-    const itemName = document.createElement('p');
-    itemName.textContent = randomCocktail.name;
+//     const itemName = document.createElement('p');
+//     itemName.textContent = randomCocktail.name;
 
-    const itemDescription = document.createElement('p');
-    itemDescription.textContent = randomCocktail.description;
+//     const itemDescription = document.createElement('p');
+//     itemDescription.textContent = randomCocktail.description;
 
-    const itemIngredients = document.createElement('ul');
-    randomCocktail.ingredients.forEach(ingredient => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${ingredient.name}: ${ingredient.quantity}`;
-        itemIngredients.appendChild(listItem);
-    });
+//     const itemIngredients = document.createElement('ul');
+//     randomCocktail.ingredients.forEach(ingredient => {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = `${ingredient.name}: ${ingredient.quantity}`;
+//         itemIngredients.appendChild(listItem);
+//     });
 
-    const itemCooking = document.createElement('p');
-    itemCooking.textContent = randomCocktail.cooking;
+//     const itemCooking = document.createElement('p');
+//     itemCooking.textContent = randomCocktail.cooking;
 
-    cocktailName.appendChild(itemName);
-    cocktailDescription.appendChild(itemDescription);
-    cocktailIngredients.appendChild(itemIngredients);
-    cocktailCooking.appendChild(itemCooking);
-}
+//     cocktailName.appendChild(itemName);
+//     cocktailDescription.appendChild(itemDescription);
+//     cocktailIngredients.appendChild(itemIngredients);
+//     cocktailCooking.appendChild(itemCooking);
+// }
 
-document.getElementById('random-cocktail-page').addEventListener('click',)
-showRandomCocktail();
+// document.getElementById('random-cocktail-page').addEventListener('click',)
+// showRandomCocktail();
 
 // document.getElementById('button').addEventListener('click', showCoctail);
