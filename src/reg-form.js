f2bd28b18b35ed0353176e719c7fe25d64ca50e0
Maryam <maryam@sosiv.io>
2023-11-19 21:33:15 +0400

    Изменение функции валидации

--- src/reg-form.js
+++ src/reg-form.js
@@ -5,89 +5,6 @@ const allInputs = document.querySelectorAll('input')
 const inputPassword = form.elements.password
 const inputPassword2 = form.elements.passwordRepeat
 
-let errors = [];
-
-
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    errors = [];
-    formValidate();
-});
-
-
-function formValidate() {
-    let requiredInputs = document.querySelectorAll('._req');
-    let values = [];
-
-    //валидация каждого обязательного поля и коллекционирование значений этих полей в массив value
-    for (let input of requiredInputs) {
-        checkValidity(input);
-        values.push(getInputName(input) + ': ' + input.value);
-    }
-
-    checkPasswords();
-
-    //если форма проходит проверку валидности(нет ошибок), то форма очищается
-    if (errors.length == 0) {
-        form.reset();
-    }
-}
-
-
-
-function checkValidity(input) {
-    let validity = input.validity;
-    let isValid = true
-
-    if (validity.patternMismatch) {
-        errors.push('Неверный формат заполнения');
-        isValid = false
-    }
-
-    else if (validity.typeMismatch) {
-        errors.push('Значение не соответствует заданному типу поля');
-        isValid = false
-    }
-
-    else if (validity.rangeOverflow) {
-        errors.push('Значение превосходит максимально допустимое');
-        isValid = false
-    }
-
-    else if (validity.rangeUnderflow) {
-        errors.push('Значение меньше минимально допустимого');
-        isValid = false
-    }
-
-    else if (validity.stepMismatch) {
-        errors.push('Недопустимое значение в соответствии с шагом');
-        isValid = false
-    }
-
-    else if (validity.tooLong) {
-        errors.push('Значение слишком длинное');
-        isValid = false
-    }
-
-    else if (validity.tooShort) {
-        errors.push('Значение слишком короткое');
-        isValid = false
-    }
-
-    else if (validity.valueMissing) {
-        errors.push('Необходимо заполнить поле');
-        isValid = false
-    }
-
-    if (isValid === false) {
-        formAddError(input)
-        addErrorText(input)
-    } else {
-        formRemoveError(input)
-        removeErrorText(input)
-    }
-}
-
 
 function getInputName(input) {
     return input.getAttribute('name');
@@ -115,11 +32,159 @@ function formRemoveError(input) {
     input.classList.remove('_error');
 }
 
+
+//=================================================================
+// По примеру Ланы:
+
+
+document.querySelector('.form__submit').onclick = validation;
+const error = document.querySelector('#error-empty');
+//Динамическая валидация полей при вводе
+
+
+//логин
+const inputLogin = document.getElementById('login');
+const errorLogin = document.querySelector('#login-error');
+const namePattern = /^[a-zA-Zа-яА-я ]{2,20}$/;
+inputLogin.oninput = function () {
+    let loginValue = inputLogin.value;
+    if (loginValue.match(namePattern) || loginValue == "") {
+        errorLogin.classList.add('invisible');
+        formRemoveError(inputLogin);
+    } else {
+        errorLogin.classList.remove('invisible');
+    }
+}
+
+
+//email
+const inputEmail = document.getElementById('email');
+const errorEmail = document.querySelector('#email-error');
+const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,30}$/i;
+inputEmail.oninput = function () {
+    let emailValue = inputEmail.value;
+    if (emailValue.match(emailPattern) || emailValue == "") {
+        errorEmail.classList.add('invisible');
+        formRemoveError(inputEmail)  //убрать подсветку поля красным
+    } else {
+        errorEmail.classList.remove('invisible');
+        formAddError(inputEmail)
+    }
+}
+
+
+//пароль первое поле
+//проверка надежности пароля
+const error_pwd1 = document.querySelector('#dinamic-pwd-error');
+const pwdPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]{8,20}$/;
+inputPassword.oninput = function () {
+    let pwdValue = inputPassword.value;
+    if (pwdValue.match(pwdPattern) || pwdPattern == "") {
+        error_pwd1.classList.add('invisible');
+    } else {
+        error_pwd1.classList.remove('invisible');
+    }
+}
+
+
+//пароль второе поле
 //проверка, что при повторном введении пароля, значения паролей были идентичными
-function checkPasswords() {
-    if (inputPassword2.value !== inputPassword.value) {
-        formAddError(inputPassword2);
-        document.getElementById(`passwordRepeat-error`).innerHTML = 'Пароли не совпадают.';
-        errors.push('Пароли не совпадают.');
+const error_pwd2 = document.querySelector('#passwordRepeat-error');
+inputPassword2.oninput = function () {
+    console.log('сверка паролей');
+    if (inputPassword2.value === inputPassword.value) {
+        error_pwd2.classList.add('invisible');
+    } else {
+        error_pwd2.classList.remove('invisible');
+    }
+}
+
+
+//функция валидации при нажатии на кнопку
+function validation(event) {
+    event.preventDefault(event); //отмена события по умолчанию
+
+    //очищение формы от ошибок
+    formRemoveEmptyError();
+
+    //валидация логина
+    validationLogin(inputLogin);
+
+    //валидация email
+    validationEmail(inputEmail);
+
+    //валидация пароля
+    validationPassword(inputPassword);
+    validationPassword(inputPassword2);
+}
+
+
+
+//Ниже функции валидайции формы при нажатии на кнопку
+
+function formRemoveEmptyError() {
+    error.classList.add('invisible');//убираем сообщение об ошибке, если оно было
+}
+
+
+function validationLogin(input) {
+    let validity = input.validity;
+    let errorPattern = errorLogin.classList.contains('invisible');//false, если есть ошибка формата ввода
+
+    if (validity.valueMissing) //значение не введено
+    {
+        formAddError(input); //подсветить поле красным
+        error.classList.remove('invisible'); // показать ошибку, что не все поля заполнены
+        return false;
+    } else if (errorPattern) //если ошибок ввода нет
+    {
+        error.classList.add('invisible'); //скрыть ошибку формата ввода
+        formRemoveError(input);  //убрать красную подсветку поля
+        return true;
+    } else {//подсветить поле с ошибкой
+        formAddError(input);
+        return false;
+    }
+}
+
+
+function validationEmail(input) {
+    let validity = input.validity;
+    let errorPattern = errorEmail.classList.contains('invisible');//false, если есть ошибка формата ввода
+
+    if (validity.valueMissing) //значение не введено
+    {
+        formAddError(input);  //подсветить поле красным
+        error.classList.remove('invisible'); // показать ошибку, что не все поля заполнены
+        return false;
+    } else if (errorPattern) //если ошибок ввода нет
+    {
+        error.classList.add('invisible'); //скрыть ошибку формата ввода
+        formRemoveError(input);  //убрать красную подсветку поля
+        return true;
+    } else {//подсветить поле с ошибкой
+        formAddError(input);
+        return false;
+    }
+}
+
+
+function validationPassword(input) {
+    let validity = input.validity;
+    let errorPattern = error_pwd1.classList.contains('invisible');//false, если есть ошибка формата ввода
+
+    if (validity.valueMissing) //значение не введено
+    {
+        formAddError(input);  //подсветить поле красным
+        error.classList.remove('invisible'); // показать ошибку, что не все поля заполнены
+        return false;
+    } else if (errorPattern) //если ошибок ввода нет
+    {
+        error.classList.add('invisible'); //скрыть ошибку формата ввода
+        formRemoveError(input);  //убрать красную подсветку поля
+        return true;
+    } else {//подсветить поле с ошибкой
+        formAddError(input);
+        return false;
     }
 }
