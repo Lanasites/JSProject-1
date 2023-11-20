ac0b1282f4675e766009bdf416d56ba3ab403c0d
Evgenia0712 <e.gunko1992@mail.ru>
2023-11-20 23:58:56 +0300

    модальное окно

--- src/ingredients.js
+++ src/ingredients.js
@@ -1,5 +1,10 @@
 import { takeAllObjects, takeOneCocktail } from './firebase.js'
 
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-content");
+const closeButton = document.getElementsByClassName("close")[0];
+
+
 function testGetData() {
   console.log('Функция загрузки данных вызвана');
   // получение промиса
@@ -23,8 +28,7 @@ function testGetData() {
     });
 }
 
-// showModal('${cocktails}')
-
+// Функция отображения ингредиентов на странице
 function print(id, name, cocktails, img) {
   const printElement = document.getElementById('container_ingredients');
   const template = `
@@ -43,21 +47,15 @@ function print(id, name, cocktails, img) {
   // console.log(elem);
 
   elem.addEventListener('click', function () {
-    showModal(cocktails);
+    openModal(cocktails);
   });
-
-  // elem.addEventListener('click', function () {  //модальное окно
-  //   modalInf(cocktails);
-  // });
-
 }
 
-const modalInfo = document.getElementById('modal_info');
-const modalElement = document.getElementById('modal');
+testGetData()
 
-// Функция отображения модального окна
-function showModal(coctails) {
-  modalElement.style.display = 'block';
+// Функция модального окна
+function openModal(coctails) {
+  modal.style.display = "block";
   coctails.forEach(element => {
     console.log(element); //название коктейля
     let promis = takeOneCocktail(element);
@@ -65,14 +63,6 @@ function showModal(coctails) {
     promis
       .then(data => {
         //console.log(data); //объект со всеми его свойствами
-        // работаем с полученными данными - объект с объектами
-        //for (let key in data) {
-        // obj - ингредиент со всеми свойствами
-        //let obj = data[key];
-        //console.log(obj);
-        //console.log(data.description);
-
-        //const printElement = document.getElementById('print');
         const template = `
           <div class="modal_item">
             <div class="modal_item-1">
@@ -87,7 +77,7 @@ function showModal(coctails) {
           </div>`
         const newElement = document.createElement('div');
         newElement.innerHTML = template;
-        modalInfo.appendChild(newElement);
+        modalContent.appendChild(newElement);
       })
       .catch(error => {
         // обрабатываем ошибку, если она возникла
@@ -96,47 +86,16 @@ function showModal(coctails) {
   });
 }
 
-testGetData()
+// Функция закрытия модального окна
+closeButton.onclick = function () {
+  modal.style.display = "none";
+  modalContent.textContent = '';
+}
 
-document.querySelector('.btn_close').addEventListener('click', function () {
-  modalElement.style.display = 'none';
-  modalInfo.textContent = '';
-})
-
-
-// Функция модального окна
-
-// function modalInf(cocktails) {
-//   cocktails.forEach(element => {
-//     console.log(element); //название коктейля
-//     let promis = takeOneCocktail(element);
-//     console.log(promis); //промис этого коктейля
-//     promis
-//       .then(data => {
-//         Swal.fire(
-//           {
-//             title: "<strong>Коктейли</u></strong>",
-//             html: `
-//             <div class="modal_item">
-//             <div class="modal_item-1">
-//             <img class="modal_item-1__img" src="${data.imageUrl}" alt="" style="width: 13vw; height: 10vw;">
-//           </div>
-//           <div class="modal_item-2">
-//             <div class="modal_item-2__name">Название: ${data.name}</div>
-//             <div class="modal_item-2__description"><span>Описание:</span> ${data.description}</div>
-//             <div class="modal_item-2__cooking"><span>Рецепт:</span> ${data.cooking}</div>
-//               <ul class="modal_item-2__id" id = ${data.id}></ul>
-//             </div>
-//           </div>
-//               `,
-//             showCloseButton: true,
-//             showCancelButton: true,
-//             focusConfirm: false,
-//           });
-//       })
-//       .catch(error => {
-//         // обрабатываем ошибку, если она возникла
-//         console.error(error);
-//       });
-//   });
-// }
+// Функция закрытия модального окна, если пользователь нажмет на область вне модального окна.
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modalContent.textContent = '';
+  }
+}
